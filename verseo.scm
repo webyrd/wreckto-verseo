5460c7e4e20574a05959c472f160bb51554473e5
William Byrd <webyrd@gmail.com>
2023-01-14 00:56:01 -0500

    working on verseo

--- verseo.scm
+++ verseo.scm
@@ -1,4 +1,28 @@
 (load "faster-miniKanren/mk-vicare.scm")
 (load "faster-miniKanren/mk.scm")
 
- 
+ ;; should prolly use CLP(SMT) for arithmetic
+
+(define verseo
+  (lambda ()
+    'TODO
+    ))
+
+
+;; Grammar from fig 1 on page 3
+
+(define expressiono
+  (lambda (expr)
+    'TODO
+    ))
+
+(define head-valueso
+  (lambda (hnf)
+    (conde
+      ((numbero hnf))
+      ;; TODO: op
+      ;; TODO: <v1, ..., vn>
+      ((fresh ()
+         (== `(lambda (,x) ,e) hnf)
+         (symbolo x)
+         (expressiono e))))))
