1340762df63988a858ac77e8ae0c83f21642a5f1
William Byrd <webyrd@gmail.com>
2023-01-14 01:11:42 -0500

    working on verseo

--- verseo.scm
+++ verseo.scm
@@ -11,18 +11,45 @@
 
 ;; Grammar from fig 1 on page 3
 
+(define integero
+  (lambda (k)
+    (numbero k)))
+
+(define variableo
+  (lambda (x)
+    (symbolo x)))
+
+(define programo
+  (lambda (prog)
+    (fresh (e)
+      ;; TODO ensure fvs(e) is empty
+      (== `(one ,e) prog)
+      (expressiono e))))
+
 (define expressiono
   (lambda (expr)
     'TODO
     ))
 
-(define head-valueso
+(define valueo
+  (lambda (v)
+    (conde
+      ((variableo v))
+      ((head-valueo v)))))
+
+(define head-valueo
   (lambda (hnf)
     (conde
-      ((numbero hnf))
-      ;; TODO: op
+      ((integerso hnf))
+      ((primopo hnf))
       ;; TODO: <v1, ..., vn>
       ((fresh ()
          (== `(lambda (,x) ,e) hnf)
          (symbolo x)
          (expressiono e))))))
+
+(define primopo
+  (lambda (op)
+    (conde
+      ((== 'gt op))
+      ((== 'add op)))))
